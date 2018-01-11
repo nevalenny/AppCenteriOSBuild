a8b150d2e89485357f274a27560c13daaa685b9a
Zen Ushida <zen_ushida@jmas.co.jp>
2018-01-11 16:25:18 +0900

    first commit

---
+++ QRReader/QRReader/QRReader.UWP/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace QRReader.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new QRReader.App(new UwpInitializer()));
+        }
+    }
+
+    public class UwpInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
---
+++ QRReader/QRReader/QRReader/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Unity;
+using Xamarin.Forms;
+
+namespace QRReader
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            
+            //NavigationService.NavigateAsync("MainPage/Reading");
+            NavigationService.NavigateAsync("MainPage");
+        }
+
+        protected override void RegisterTypes()
+        {
+
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+            
+        }
+    }
+}
